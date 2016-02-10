3ad64a1ac2f9f2adc3118f989dba3f960103f2b8
hessfr <frederik.hess@gmail.com>
2016-02-10 09:13:49 +0100

    Finished logic of show/hide menu buttons

--- www/js/MenuController.js
+++ www/js/MenuController.js
@@ -12,25 +12,29 @@ angular.module('kApp.MenuController', [])
     // Wait for animation to finish:
     $timeout(function () {
         document.getElementById('fab-menu-main').classList.toggle('on');
-        document.getElementById('fab-menu-left').classList.toggle('on');
-        document.getElementById('fab-menu-middle').classList.toggle('on');
-        document.getElementById('fab-menu-right').classList.toggle('on');
     }, 200);
     $scope.mainMenuClicked = function() {
         console.log("mainMenuClicked");
-        $ionicSideMenuDelegate.toggleLeft();
+        if ($ionicSideMenuDelegate.isOpen()) {
+            $scope.hideMenu();
+        } else {
+            $scope.showMenu();
+        }
     };
     $scope.leftMenuClicked = function() {
         console.log("leftMenuClicked");
         $state.go('app.page1');
+        $scope.hideMenu();
     };
     $scope.middleMenuClicked = function() {
         console.log("middleMenuClicked");
         $state.go('app.page2');
+        $scope.hideMenu();
     };
     $scope.rightMenuClicked = function() {
         console.log("rightMenuClicked");
         $state.go('app.page3');
+        $scope.hideMenu();
     };
 
     // Set Motion
@@ -39,4 +43,21 @@ angular.module('kApp.MenuController', [])
     // Set Ink
     ionicMaterialInk.displayEffect();
 
+    $scope.showMenu = function() {
+        if (!$ionicSideMenuDelegate.isOpen()) {
+            $ionicSideMenuDelegate.toggleLeft();
+        }
+        document.getElementById('fab-menu-left').classList.toggle('on', true);
+        document.getElementById('fab-menu-middle').classList.toggle('on', true);
+        document.getElementById('fab-menu-right').classList.toggle('on', true);
+    };
+
+    $scope.hideMenu = function() {
+        if ($ionicSideMenuDelegate.isOpen()) {
+            $ionicSideMenuDelegate.toggleLeft();
+        }
+        document.getElementById('fab-menu-left').classList.toggle('on', false);
+        document.getElementById('fab-menu-middle').classList.toggle('on', false);
+        document.getElementById('fab-menu-right').classList.toggle('on', false);
+    };
 })
