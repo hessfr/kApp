4ef138589edaededca5258dbbac7429a3f47dea5
hessfr <frederik.hess@gmail.com>
2016-02-10 08:39:17 +0100

    added missing files

---
+++ www/js/Page1Controller.js
@@ -0,0 +1,24 @@
+/* global angular, document, window */
+'use strict';
+
+angular.module('kApp.Page1Controller', [])
+
+.controller('Page1Controller', function($scope, $stateParams, $timeout, ionicMaterialInk, ionicMaterialMotion) {
+    // Set Header
+    $scope.$parent.showHeader();
+    $scope.$parent.clearFabs();
+    $scope.$parent.setHeaderFab('left');
+
+    // Delay expansion
+    $timeout(function() {
+        $scope.isExpanded = true;
+        $scope.$parent.setExpanded(true);
+    }, 300);
+
+    // Set Motion
+    ionicMaterialMotion.fadeSlideInRight();
+
+    // Set Ink
+    ionicMaterialInk.displayEffect();
+
+})
